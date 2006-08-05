8df9c7318a0e3c6d88ca8667be18c3fdfd8a974f
nc9005ar <nc9005ar>
2006-08-05 15:52:15 +0000

    restructuring

--- src/com/mobiletech/imageconverter/jaiextensions/ToIndexColorImageType.java
+++
@@ -1,9 +0,0 @@
-package com.mobiletech.imageconverter.jaiextensions;
-
-import javax.media.jai.EnumeratedParameter;
-
-public final class ToIndexColorImageType extends EnumeratedParameter {
-	public ToIndexColorImageType(String name, int value) {
-        super(name, value);
-    }
-}
