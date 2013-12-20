a3a98c5fb18410038961925cbd944509655a7e09
Eivinn Hustveit <eh@mobiletech.no>
2013-12-20 12:36:28 +0100

    use ImageIO

--- src/main/java/com/mobiletech/imageconverter/writers/JPEGImageWriter.java
+++ src/main/java/com/mobiletech/imageconverter/writers/JPEGImageWriter.java
@@ -6,64 +6,79 @@ import java.io.IOException;
 
 import com.mobiletech.imageconverter.exception.ImageConverterException;
 import com.mobiletech.imageconverter.vo.ImageConverterParams;
-import com.sun.media.jai.codec.JPEGEncodeParam;
-import com.sun.media.jai.codecimpl.JPEGCodec;
 import com.sun.media.jai.codecimpl.JPEGImageEncoder;
+import java.util.Iterator;
+import java.util.Locale;
+import javax.imageio.IIOImage;
+import javax.imageio.ImageIO;
+import javax.imageio.ImageWriteParam;
+import javax.imageio.ImageWriter;
+import javax.imageio.plugins.jpeg.JPEGImageWriteParam;
+import javax.imageio.stream.ImageOutputStream;
 
 public class JPEGImageWriter implements DexImageWriter {
-	private byte[] result= null;
-	private ImageConverterParams params = null;
-	private ByteArrayOutputStream output = null;
-	
-	private JPEGImageWriter(){}
-	
-	public JPEGImageWriter(ImageConverterParams params){
-		this.params = params;
-	}
-	public boolean canWriteMore(){
-		return false;
-	}
-	public byte[] getByte() throws ImageConverterException{
-		try {              
-			output.flush();
-			result = output.toByteArray();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		return result;
-	}
-	public void dispose(){
-        try{                
-            output.close();
-        } catch(IOException ignored){}
-        output = null;
-	}
-	public void writeNext(BufferedImage image) throws ImageConverterException{
-		output = new ByteArrayOutputStream();
-		JPEGImageEncoder enc = JPEGCodec.createJPEGEncoder(output);
-        
-        if(params.getJPEGCompressionQuality() != 0){                
-            JPEGEncodeParam param = JPEGCodec.getDefaultJPEGEncodeParam(image);
-            if(params.getJPEGCompressionQuality() > 0){
-                param.setQuality(params.getJPEGCompressionQuality());
-            }
-            //if(params.useSubsampling()){
-                param.setHorizontalSubsampling(0, 1); 
-                param.setHorizontalSubsampling(1, 1); 
-                param.setHorizontalSubsampling(2, 1); 
-                param.setVerticalSubsampling(0, 1); 
-                param.setVerticalSubsampling(1, 1); 
-                param.setVerticalSubsampling(2, 1);
-            //}
-            enc.setJPEGEncodeParam(param);
-        }
+
+    private byte[] result = null;
+    private ImageConverterParams params = null;
+    private ByteArrayOutputStream output = null;
+
+    private JPEGImageWriter() {
+    }
+
+    public JPEGImageWriter(ImageConverterParams params) {
+        this.params = params;
+    }
+
+    public boolean canWriteMore() {
+        return false;
+    }
+
+    public byte[] getByte() throws ImageConverterException {
         try {
-            enc.encode(image);
+            output.flush();
+            result = output.toByteArray();
         } catch (IOException e) {
-            throw new ImageConverterException(ImageConverterException.Types.IO_ERROR,"IOException thrown when writing encoded image ",e);
-        } finally {
-            enc = null;
+            // TODO Auto-generated catch block
+            e.printStackTrace();
         }
-	}
+        return result;
+    }
+
+    public void dispose() {
+        try {
+            output.close();
+        } catch (IOException ignored) {
+        }
+        output = null;
+    }
+
+    public void writeNext(BufferedImage image) throws ImageConverterException {
+        output = new ByteArrayOutputStream();
+        ImageWriter imgWriter = null;
+        try {
+            ImageOutputStream ios = ImageIO.createImageOutputStream(output);
+            JPEGImageEncoder enc;
+
+            Iterator<ImageWriter> writers = ImageIO.getImageWritersByFormatName("jpg");
+            if (writers != null) {
+                imgWriter = writers.next();
+            }
+
+            ImageWriteParam writeParam = imgWriter.getDefaultWriteParam();
+            writeParam = new JPEGImageWriteParam(Locale.US);
+            ((JPEGImageWriteParam) writeParam).setOptimizeHuffmanTables(true);
+            if (params.getJPEGCompressionQuality() != 0 && writeParam.canWriteCompressed()) {
+                writeParam.setCompressionMode(JPEGImageWriteParam.MODE_EXPLICIT);
+                writeParam.setCompressionQuality(params.getJPEGCompressionQuality());
+            }
+            IIOImage img = new IIOImage(image, null, null);
+            imgWriter.write(null, img, writeParam);
+        } catch (IOException e) {
+            throw new ImageConverterException(ImageConverterException.Types.IO_ERROR, "IOException thrown when writing encoded image ", e);
+        } finally {
+            if (imgWriter != null) {
+                imgWriter.dispose();
+            }
+        }
+    }
 }
